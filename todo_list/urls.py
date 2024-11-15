e5d95ad88dbeed961eb04f21aa1011d4d1b739e2
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-11-15 11:46:23 +0200

    Add project URLs by splitting into project-level and app-level URLs

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("doer.urls", namespace="doer")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
